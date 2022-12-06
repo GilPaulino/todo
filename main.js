f06bda02e10a7997fd98406c44edd6bbea4a6360
Gilberto Paulino <gilbertopaulinoo@gmail.com>
2022-12-06 17:53:56 -0300

    Atualização :+1:
    Javascript: Adição da Função contador;
    Atualização da tela ao adicionar ou remover um item.

--- main.js
+++ main.js
@@ -1,90 +1,102 @@
-const form = document.getElementById("nova-tarefa")
-const contador = document.getElementsByClassName('contador')
-const lista = document.getElementById("lista") 
-const itens = JSON.parse(localStorage.getItem("itens")) || []
+const form = document.getElementById("nova-tarefa");
+const contador = document.getElementById("contadorTarefa");
+const contadorC = document.getElementById("contadorCriada");
+const checkbox = document.getElementById("checkbox");
+const lista = document.getElementById("lista");
+const itens = JSON.parse(localStorage.getItem("itens")) || [];
 
+itens.forEach((elemento) => {
+  criaTarefa(elemento);
+});
 
+var btn = document.querySelector("#refresh");
+btn.addEventListener("click", function () {
+  location.reload();
+});
 
-itens.forEach((elemento) => {    
-    criaTarefa(elemento)          
-})
+form.addEventListener("submit", (evento) => {
+  evento.preventDefault();
+  const nome = evento.target.elements["nome"];
 
+  const existe = itens.find((elemento) => elemento.nome === nome.value);
+  const tarefas = {
+    nome: nome.value,
+  };
 
+  if (existe) {
+    tarefas.id = existe.id;
+    itens[itens.findIndex((elemento) => elemento.id === existe.id)] = tarefas;
+  } else {
+    tarefas.id = itens[itens.length - 1] ? itens[itens.length - 1].id + 1 : 0;
+    criaTarefa(tarefas);
+    itens.push(tarefas);
+  }
 
-form.addEventListener("submit", (evento) =>{
-    evento.preventDefault()
-    const nome = evento.target.elements['nome']
-    
-    const existe = itens.find( elemento => elemento.nome === nome.value)
-    const tarefas = {
-        "nome": nome.value
-    }
+  localStorage.setItem("itens", JSON.stringify(itens));
 
-    if (existe){
-        tarefas.id = existe.id
-        itens[itens.findIndex(elemento => elemento.id === existe.id)] = tarefas
-        } else {
-        tarefas.id = itens[itens.length -1] ? (itens[itens.length-1]).id + 1 : 0;
-        criaTarefa(tarefas)
-        itens.push(tarefas)        
-    }  
-    
-    localStorage.setItem("itens", JSON.stringify(itens))
+  nome.value = "";
+});
 
-    nome.value=""
-}) 
+function criaTarefa(item) {
+  const novaTarefa = document.createElement("div");
+  novaTarefa.classList.add("tarefa");
 
+  const textoTarefa = document.createElement("p");
+  textoTarefa.innerHTML = item.nome;
+  textoTarefa.dataset.id = item.id;
 
-function criaTarefa(item){
-    
-    const novaTarefa = document.createElement('div')
-    novaTarefa.classList.add("tarefa")  
-    
-    const textoTarefa = document.createElement('p')   
-    textoTarefa.innerHTML = item.nome
-    textoTarefa.dataset.id = item.id  
+  const deleteItem = document.createElement("i");
+  deleteItem.classList.add("fa-regular");
+  deleteItem.classList.add("fa-trash-can");
 
-    const deleteItem = document.createElement('i')
-    deleteItem.classList.add('fa-regular')
-    deleteItem.classList.add('fa-trash-can') 
+  lista.appendChild(novaTarefa);
+  novaTarefa.innerHTML = `<input type="checkbox" class="checkbox" id="checkbox">`;
+  novaTarefa.appendChild(textoTarefa);
+  novaTarefa.appendChild(botaoDeleta(deleteItem));
 
+  let quantidadeItem = itens.length;
+  const quantidade = quantidadeItem;
+    contador.innerText = quantidade;
 
-    lista.appendChild(novaTarefa)
-    novaTarefa.innerHTML = `<input type="checkbox" class="checkbox" id="checkbox">`
-    novaTarefa.appendChild(textoTarefa)
-    novaTarefa.appendChild(botaoDeleta(deleteItem))  
-    
+  localStorage.setItem("itens", JSON.stringify(itens));
 }
 
-/*function contadorTarefa(item) {
-
-    const quantidadeTarefa = document.getElementsByClassName('tarefa')
-    const quantidade = document.createElement('div')
-    quantidade.classList.add('contadorTarefa')
+function checkItem(id) {
+  checkbox.addEventListener("click", function () {
+    checkedItem(this.parentNode, id);
     
-    quantidade.innerText = quantidadeTarefa.length
+  });
 
-    
+  return checkbox;
+}
 
-    contador.appendChild(quantidade)
-    
-} */
+function checkedItem(tag, id) {
+  tag.status();
+  itens.status(itens.findIndex((elemento) => elemento.id === id).cheked);
+  localStorage.setItem("itens", JSON.stringify(itens));
+}
 
 function botaoDeleta(id) {
-    const deleteItem = document.createElement('i')
-    deleteItem.classList.add('fa-regular')
-    deleteItem.classList.add('fa-trash-can')
+  const deleteItem = document.createElement("i");
+  deleteItem.classList.add("fa-regular");
+  deleteItem.classList.add("fa-trash-can");
 
-    deleteItem.addEventListener("click", function(){
-        deletaTarefa(this.parentNode, id)
-    })
+  deleteItem.addEventListener("click", function () {
+    deletaTarefa(this.parentNode, id);
+    let quantidadeItem = itens.length;
+    const quantidade = quantidadeItem;
+    contador.innerText = quantidade;
+  });
 
-    return deleteItem
+  return deleteItem;
 }
 
-function deletaTarefa(tag, id){
-    tag.remove()
-    itens.splice(itens.findIndex(elemento => elemento.id ===id), 1) 
-    
-    localStorage.setItem("itens", JSON.stringify(itens))
+function deletaTarefa(tag, id) {
+  tag.remove();
+  itens.splice(
+    itens.findIndex((elemento) => elemento.id === id),
+    1
+  );
+
+  localStorage.setItem("itens", JSON.stringify(itens));
 }
