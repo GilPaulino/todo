bc6de8fc761e138c6fe83f2203d31dab49f314a8
Gilberto Paulino <gilbertopaulinoo@gmail.com>
2022-12-14 16:35:24 -0300

    Atualização Refatoração
    Javascript: Criando e Armazenando tarefas na lista;

--- main.js
+++ main.js
@@ -1,25 +1,50 @@
-// const adicionaTarefa = ( afazer, condicao='') => {
+const todasTarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
 
-// var descricao = document.querySelector('#descricao').value;
+const containerDescricao = document.querySelector("#containerDescricao");
+containerDescricao.addEventListener("submit", (adicionaTarefa) => {
+  adicionaTarefa.preventDefault();
 
-// const tarefa = document.createElement('div');
-// tarefa.classList.add('tarefa');
-// tarefa.innerHTML = `
-//     <label class="containerVerificacao">
-//     <input type="checkbox" ${condicao}>
-//     <span class="verifica" ></span> </label>    
+  const descricao = adicionaTarefa.target.elements["descricao"];
 
-//     <p id="descricaoTarefa"class="descricaoTarefa"> ${afazer} </p>
+  const existeTarefa = todasTarefas.find((lista) => lista.descricao === descricao.value);
 
-//     <i class="fa-regular fa-trash-can"></i>
-// `
-// document.getElementById('tarefas').appendChild(tarefa);
+  const tarefas = {        
+    tarefas: descricao.value,
+    checked: false,
+  };
 
-// }
+  if (existeTarefa){    
+    tarefas.id = existeTarefa.id
+    todasTarefas[itens.findIndex((lista) => lista.id === existeTarefa.id)] = tarefas;
+  }else{
+    tarefas.id = todasTarefas[todasTarefas.length - 1] ? todasTarefas[todasTarefas.length - 1].id +1 : 0;
+    criaTarefa(tarefas); 
+    todasTarefas.push(tarefas);
+} 
 
-// const descricaoTarefa = document.getElementById('descricaoTarefa');
+  localStorage.setItem("tarefas", JSON.stringify(todasTarefas));
 
-// descricaoTarefa.addEventListener('submit', (adcionaTarefa)=> {
-//     adcionaTarefa.preventDefault()
-//     console.log('funcionou')
-// })
+  descricao.value = "";
+});
+
+const criaTarefa = (afazer, condicao= "") => {
+  const tarefa = document.createElement("div");
+  tarefa.classList.add("tarefa");
+  tarefa.dataset.id = afazer.id
+  tarefa.innerHTML = `
+    <label class="containerVerificacao">
+    <input type="checkbox" ${condicao}>
+    <span class="verifica" ></span> </label>    
+
+    <p id="descricaoTarefa"class="descricaoTarefa">${afazer.tarefas}</p>
+
+    <i class="fa-regular fa-trash-can"></i>`;
+
+  document.getElementById("tarefas").appendChild(tarefa);
+
+};
+
+
+todasTarefas.map((tarefas) => {
+    criaTarefa(tarefas);  
+});
